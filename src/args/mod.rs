03f09571e379edf4f7e4bb97920ff6cf65e5078a
Viktor Sandström <sandstrom.viktor@gmail.com>
2025-08-10 11:33:51 +0200

    config impl
    implemented Config struct impl, and Argument validation parsing

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -14,11 +14,11 @@ pub(crate) struct Args {
   pub password: Option<String>,
 
   /// IceCast server URL
-  #[arg(long)]
+  #[arg(long, value_parser=validate_ip)]
   pub url: Option<String>,
 
   /// IceCast server port
-  #[arg(long)]
+  #[arg(long, value_parser=validate_port)]
   pub port: Option<u16>,
 
   /// Optional custom filename of local copy
@@ -31,4 +31,22 @@ pub(crate) struct Args {
 
   #[arg(long)]
   pub reset_config: bool
+}
+
+
+fn validate_ip(s: &str) -> Result<String, String> {
+  if is_ip::is_ip(s) {
+    return Ok(s.to_string());
+  }
+  Err("IP is not valid".to_owned())
+}
+
+fn validate_port(p: &str) -> Result<u16, String> {
+  let p = match p.parse::<u16>() {
+    Ok(n) => n,
+    Err(e) => return Err(format!("Unable to parse as number: {e}"))
+  };
+
+  if (1..=0xFFFF).contains(&p) { return Ok(p); } 
+  Err("Port is not within valid range: 1 - 65535".to_owned())
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,8 +1,19 @@
 use super::{Arc, Local};
+use regex::Regex;
 
 /// Formats the file name of output file, using current local datetime.
 /// If no filename is given in cli arguments. default = `tau_[datetime].ogg`
 pub fn format_filename(filename: Option<String>) -> Arc<String> {
   let now = Local::now().format("%d-%m-%Y_%H_%M_%S").to_string();
   Arc::new(filename.map(|f| format!("{f}.ogg")).unwrap_or_else(|| format!("tau_{}.ogg", now)))
+}
+
+
+pub fn validate_port(p: &str) -> Result<u16, String> {
+    let port = p.parse::<u16>().map_err(|_| "Invalid port number")?;
+    if !(1..=0xFFFF).contains(&port) {
+        Ok(port)
+    } else {
+        Err("Port number must be between 1 and 65535".to_owned())
+    }
 }
